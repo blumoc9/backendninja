615be65029426eec6a92e8c351c7c5561a06fba1
blumoc9 <blumoc9@blumoc9-K55N>
2017-11-08 23:48:01 -0300

    Otra Forma de request

--- src/main/java/com/udemy/backendninja/controller/Example2Controller.java
+++ src/main/java/com/udemy/backendninja/controller/Example2Controller.java
@@ -2,6 +2,7 @@ package com.udemy.backendninja.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
@@ -18,6 +19,13 @@ public class Example2Controller {
 	public ModelAndView request1(@RequestParam(name="nm",required=false,defaultValue=NULL)String name){
 		ModelAndView modelAndView = new ModelAndView(EXAMPLE2_VIEW);
 		modelAndView.addObject("nm_in_model",name);
+		return modelAndView;
+	}
+	
+	@GetMapping("/request2/{nm}")
+	public ModelAndView request2(@PathVariable("nm") String name){
+		ModelAndView modelAndView = new ModelAndView(EXAMPLE2_VIEW);
+		modelAndView.addObject("nm_in_model", name);
 		return modelAndView;
 	}
 }
